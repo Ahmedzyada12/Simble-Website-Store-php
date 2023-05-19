0405a9b64dc0605f513221334ce345315b8dffac
Ahmedzyada12 <126186881+Ahmedzyada12@users.noreply.github.com>
2023-05-19 22:30:50 +0300

    Add files via upload

---
+++ inc/data.php
@@ -0,0 +1,85 @@
+<?php
+    $products = [];
+
+    $products[1] = [
+        "title" => "Oil",
+        "img" =>"1.jpg",
+        "price" => 30,
+        "description" => "Oil ipsum dolor, sit amet consectetur adipisicing elit. Natus omnis expedita dignissimos nesciunt, tempora porro facilis enim odio soluta obcaecati culpa laudantium, debitis ipsam molestias, at in ipsa repellat autem?"
+    ];
+
+    $products[2] = [
+        "title" => "Pink Bag",
+        "img" =>"2.jpg",
+        "price" => 200,
+        "description" => "Pink Bag ipsum dolor, sit amet consectetur adipisicing elit. Natus omnis expedita dignissimos nesciunt, tempora porro facilis enim odio soluta obcaecati culpa laudantium, debitis ipsam molestias, at in ipsa repellat autem?"
+    ];
+
+    $products[3] = [
+        "title" => "Olive T-shirt",
+        "img" =>"3.jpg",
+        "price" => 125,
+        "description" => "Olive T-shirt ipsum dolor, sit amet consectetur adipisicing elit. Natus omnis expedita dignissimos nesciunt, tempora porro facilis enim odio soluta obcaecati culpa laudantium, debitis ipsam molestias, at in ipsa repellat autem?"
+    ];
+
+    $products[4] = [
+        "title" => "Pink Bag",
+        "img" =>"4.jpg",
+        "price" => 300,
+        "description" => "Black Shose ipsum dolor, sit amet consectetur adipisicing elit. Natus omnis expedita dignissimos nesciunt, tempora porro facilis enim odio soluta obcaecati culpa laudantium, debitis ipsam molestias, at in ipsa repellat autem?"
+    ];
+
+    $products[5] = [
+        "title" => "Blue T-shirt",
+        "img" =>"5.jpg",
+        "price" => 125,
+        "description" => "Blue T-shirt ipsum dolor, sit amet consectetur adipisicing elit. Natus omnis expedita dignissimos nesciunt, tempora porro facilis enim odio soluta obcaecati culpa laudantium, debitis ipsam molestias, at in ipsa repellat autem?"
+    ];
+
+    $products[6] = [
+        "title" => "Brown Hand Bag",
+        "img" =>"6.jpg",
+        "price" => 150,
+        "description" => "Brown Hand Bag ipsum dolor, sit amet consectetur adipisicing elit. Natus omnis expedita dignissimos nesciunt, tempora porro facilis enim odio soluta obcaecati culpa laudantium, debitis ipsam molestias, at in ipsa repellat autem?"
+    ];
+
+    $products[7] = [
+        "title" => "Women Bag",
+        "img" =>"7.jpg",
+        "price" => 150,
+        "description" => "Women Bag ipsum dolor, sit amet consectetur adipisicing elit. Natus omnis expedita dignissimos nesciunt, tempora porro facilis enim odio soluta obcaecati culpa laudantium, debitis ipsam molestias, at in ipsa repellat autem?"
+    ];
+    $products[8] = [
+        "title" => "watch",
+        "img" =>"8.jpg",
+        "price" => 1500,
+        "description" => "watch ipsum dolor, sit amet consectetur adipisicing elit. Natus omnis expedita dignissimos nesciunt, tempora porro facilis enim odio soluta obcaecati culpa laudantium, debitis ipsam molestias, at in ipsa repellat autem?"
+    ];
+    $products[9] = [
+        "title" => "sport-shirt",
+        "img" =>"9.jpg",
+        "price" => 450,
+        "description" => "sport-shirt ipsum dolor, sit amet consectetur adipisicing elit. Natus omnis expedita dignissimos nesciunt, tempora porro facilis enim odio soluta obcaecati culpa laudantium, debitis ipsam molestias, at in ipsa repellat autem?"
+    ];
+    $products[10] = [
+        "title" => "shoes",
+        "img" =>"10.jpg",
+        "price" => 1500,
+        "description" => "shoes ipsum dolor, sit amet consectetur adipisicing elit. Natus omnis expedita dignissimos nesciunt, tempora porro facilis enim odio soluta obcaecati culpa laudantium, debitis ipsam molestias, at in ipsa repellat autem?"
+    ];
+    $products[11] = [
+        "title" => "phone",
+        "img" =>"11.jpg",
+        "price" => 10000,
+        "description" => "phone ipsum dolor, sit amet consectetur adipisicing elit. Natus omnis expedita dignissimos nesciunt, tempora porro facilis enim odio soluta obcaecati culpa laudantium, debitis ipsam molestias, at in ipsa repellat autem?"
+    ];
+    $products[12] = [
+        "title" => "",
+        "img" =>"12.jpg",
+        "price" => 6500,
+        "description" => "x-shawmi ipsum dolor, sit amet consectetur adipisicing elit. Natus omnis expedita dignissimos nesciunt, tempora porro facilis enim odio soluta obcaecati culpa laudantium, debitis ipsam molestias, at in ipsa repellat autem?"
+    ];
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+include('./inc/header.php');
+include('./inc/nav.php');
+include('./inc/slide.php');
+include('./inc/Home.php');
+include('./inc/footer.php');
+?>
